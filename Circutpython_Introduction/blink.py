b0ba933f297fd8d58de9364afb4d535c3485b55f
Ben <si_ben_tran@hotmail.com>
2023-03-01 17:03:51 +0100

    first iot test

---
+++ Circutpython_Introduction/blink.py
@@ -0,0 +1,14 @@
+import board
+import digitalio
+import time
+
+# setup
+led = digitalio.DigitalInOut(board.RED_LED)  # general-purpose RED LED on Pin D3
+led.direction = digitalio.Direction.OUTPUT
+
+# main loop
+while True:
+    led.value = True
+    time.sleep(1)
+    led.value = False
+    time.sleep(1)
